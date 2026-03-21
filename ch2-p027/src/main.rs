0488ad8dc3e560a9887e58951fe31dc8cd55b651
David Wallace Croft <david@CroftSoft.com>
2026-03-20 21:42:24 -0500

    unit test

--- ch2-p027/src/main.rs
+++ ch2-p027/src/main.rs
@@ -19,3 +19,37 @@ fn main() {
 
   println!("Linfa dataset: {dataset:?}");
 }
+
+#[cfg(test)]
+mod test {
+  use super::*;
+
+  #[test]
+  fn test() {
+    let features = array![
+      [
+        1., 2.
+      ],
+      [
+        3., 4.
+      ]
+    ];
+
+    let targets = array![
+      0, 1
+    ];
+
+    let dataset = Dataset::new(features, targets);
+
+    let actual = &format!("{dataset:?}");
+
+    assert_eq!(
+      actual,
+      "DatasetBase { records: [[1.0, 2.0],\n [3.0, 4.0]], shape=[2, 2], \
+      strides=[2, 1], layout=Cc (0x5), const ndim=2, targets: [0, 1], \
+      shape=[2], strides=[1], layout=CFcf (0xf), const ndim=1, weights: [], \
+      shape=[0], strides=[0], layout=CFcf (0xf), const ndim=1, \
+      feature_names: [] }"
+    );
+  }
+}
