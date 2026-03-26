78ca60fcbeafb49e0f16d812dc2769cdad70f524
David Wallace Croft <david@CroftSoft.com>
2026-03-25 20:00:09 -0500

    unit test

--- ch2-pr2/src/main.rs
+++ ch2-pr2/src/main.rs
@@ -1,9 +1,32 @@
 use ::ndarray::Array;
+use ::ndarray::ArrayBase;
+use ::ndarray::Dim;
+use ::ndarray::OwnedRepr;
 
 fn main() {
-  let arr = Array::from_vec(vec![
+  let arr: ArrayBase<OwnedRepr<i32>, Dim<[usize; 1]>> = Array::from_vec(vec![
     5, 10, 15,
   ]);
 
   println!("Array: {arr:?}");
+}
+
+#[cfg(test)]
+mod test {
+  use super::*;
+
+  #[test]
+  fn test() {
+    let arr: ArrayBase<OwnedRepr<i32>, Dim<[usize; 1]>> =
+      Array::from_vec(vec![
+        5, 10, 15,
+      ]);
+
+    let actual: &String = &format!("{arr:?}");
+
+    assert_eq!(
+      actual,
+      "[5, 10, 15], shape=[3], strides=[1], layout=CFcf (0xf), const ndim=1"
+    );
+  }
 }
