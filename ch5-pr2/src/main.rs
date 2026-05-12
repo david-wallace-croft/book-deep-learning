9a9b5ff58e07a74d9327f9676046fd392f391cdf
David Wallace Croft <david@CroftSoft.com>
2026-05-12 07:00:01 -0500

    ch5-pr2

---
+++ ch5-pr2/src/main.rs
@@ -0,0 +1,14 @@
+use ::autodiff::*;
+
+fn main() {
+  let f = |v: &[FT<f64>]| -> FT<f64> { (v[0] - v[1]) * (v[0] - v[1]) };
+
+  let v: Vec<F<f64, f64>> = vec![
+    F1::var(3.),
+    F1::cst(2.),
+  ];
+
+  let dual: F<f64, f64> = f(&v);
+
+  println!("{dual}");
+}
