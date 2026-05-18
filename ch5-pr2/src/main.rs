a2a404159a72fa4ebc0417579c4d651c7cd815b5
David Wallace Croft <david@CroftSoft.com>
2026-05-17 20:44:01 -0500

    y_hat

--- ch5-pr2/src/main.rs
+++ ch5-pr2/src/main.rs
@@ -4,8 +4,8 @@ fn main() {
   let f = |v: &[FT<f64>]| -> FT<f64> { (v[0] - v[1]) * (v[0] - v[1]) };
 
   let v: Vec<F<f64, f64>> = vec![
-    F1::var(3.),
-    F1::cst(2.),
+    F1::cst(3.),
+    F1::var(2.),
   ];
 
   let dual: F<f64, f64> = f(&v);
