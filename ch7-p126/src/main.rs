f08079cbbaf2f362d83c5e4da60f3524204ea59e
David Wallace Croft <david@CroftSoft.com>
2026-06-04 19:28:09 -0500

    parameters

--- ch7-p126/src/main.rs
+++ ch7-p126/src/main.rs
@@ -1,8 +1,37 @@
 #![expect(dead_code)]
+#![expect(unused_mut)]
+#![expect(unused_variables)]
 
-use ::tch::{Device, Kind, Tensor};
+use ::tch::nn::{
+  self, Adam, Linear, Optimizer, OptimizerConfig, Path, VarStore,
+};
+use ::tch::{Device, Kind, TchError, Tensor};
 
-fn main() {
+fn main() -> Result<(), TchError> {
+  let device: Device = Device::cuda_if_available();
+
+  let vs: VarStore = VarStore::new(device);
+
+  let root: &Path<'_> = &vs.root();
+
+  let vocab: i64 = 6;
+
+  let hidden: i64 = 16;
+
+  let t_steps: i64 = 8;
+
+  let batch: i64 = 32;
+
+  let epochs: i64 = 120;
+
+  let wx: Linear = nn::linear(root / "wx", vocab, hidden, Default::default());
+
+  let wh: Linear = nn::linear(root / "wh", hidden, hidden, Default::default());
+
+  let wy: Linear = nn::linear(root / "wy", hidden, vocab, Default::default());
+
+  let mut opt: Optimizer = Adam::default().build(&vs, 1e-3)?;
+
   todo!()
 }
 
