0464c2dbe16b08c32eae2164eb03f47a39c0791a
David Wallace Croft <david@CroftSoft.com>
2026-06-30 21:54:17 -0500

    removed unused code

--- ch7-p133/src/main.rs
+++ ch7-p133/src/main.rs
@@ -21,12 +21,6 @@ fn main() -> Result<(), TchError> {
 
   let root_path: &Path<'_> = &var_store.root();
 
-  // let wx: Linear = nn::linear(root / "wx", vocab, hidden, Default::default());
-
-  // let wh: Linear = nn::linear(root / "wh", hidden, hidden, Default::default());
-
-  // let wy: Linear = nn::linear(root / "wy", hidden, vocab, Default::default());
-
   let rnn_config: RNNConfig = RNNConfig {
     num_layers: 1,
     bidirectional: false,
@@ -56,27 +50,6 @@ fn main() -> Result<(), TchError> {
   for epoch in 1..=EPOCH_COUNT {
     let (x_one_hot, y_idx): (Tensor, Tensor) =
       make_batch(SEQUENCES_PER_BATCH, device);
-
-    // let mut h: Tensor = Tensor::zeros(
-    //   [
-    //     batch, hidden,
-    //   ],
-    //   (Kind::Float, device),
-    // );
-
-    // let mut logits_per_t: Vec<Tensor> = Vec::with_capacity(t_steps as usize);
-
-    // for t in 0..t_steps {
-    //   let x_t: Tensor = x_oh.narrow(1, t, 1).squeeze_dim(1);
-
-    //   let a: Tensor = x_t.apply(&wx) + h.apply(&wh);
-
-    //   h = a.tanh();
-
-    //   let logits_t: Tensor = h.apply(&wy);
-
-    //   logits_per_t.push(logits_t);
-    // }
 
     let (h_seq, _state): (Tensor, LSTMState) = lstm.seq(&x_one_hot);
 
@@ -111,26 +84,6 @@ fn evaluate(
   wy: &Linear,
 ) -> Result<(), TchError> {
   let (x_eval_one_hot, y_eval_idx): (Tensor, Tensor) = make_batch(1, device);
-
-  // let mut eval_logits_per_t: Vec<Tensor> =
-  //   Vec::with_capacity(t_steps as usize);
-
-  // let mut h_eval: Tensor = Tensor::zeros(
-  //   [
-  //     1, hidden,
-  //   ],
-  //   (Kind::Float, device),
-  // );
-
-  // for t in 0..t_steps {
-  //   let x_t: Tensor = x_eval_oh.narrow(1, t, 1).squeeze_dim(1);
-
-  //   h_eval = (x_t.apply(&wx) + h_eval.apply(&wh)).tanh();
-
-  //   eval_logits_per_t.push(h_eval.apply(&wy));
-  // }
-
-  // let logits_eval: Tensor = Tensor::stack(&eval_logits_per_t, 1);
 
   let (h_eval, _st): (Tensor, LSTMState) = lstm.seq(&x_eval_one_hot);
 
