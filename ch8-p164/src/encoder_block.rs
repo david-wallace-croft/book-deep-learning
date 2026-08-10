e8fb2427f616d49c416626ee9d2553042dea6ae9
David Wallace Croft <david@CroftSoft.com>
2026-08-10 15:52:47 -0500

    tiny nlp transformer impl

--- ch8-p164/src/encoder_block.rs
+++ ch8-p164/src/encoder_block.rs
@@ -1,9 +1,29 @@
 use super::mhsa::Mhsa;
-use ::tch::nn::{LayerNorm, Sequential};
+use ::tch::Tensor;
+use ::tch::nn::{LayerNorm, Path, Sequential};
 
 pub struct EncoderBlock {
   ln1: LayerNorm,
   ln2: LayerNorm,
   attn: Mhsa,
   ffn: Sequential,
+}
+
+impl EncoderBlock {
+  pub fn new(
+    vs: &Path,
+    d_model: i64,
+    n_heads: i64,
+    d_ff: i64,
+  ) -> Self {
+    todo!()
+  }
+
+  pub fn forward(
+    &self,
+    x: &Tensor,
+    train: bool,
+  ) -> Tensor {
+    todo!()
+  }
 }
--- ch8-p164/src/tiny_nlp_transformer.rs
+++ ch8-p164/src/tiny_nlp_transformer.rs
@@ -1,6 +1,6 @@
 use super::encoder_block::EncoderBlock;
-use ::tch::nn::{Embedding, LayerNorm, Linear, Path};
-use ::tch::{Device, Tensor};
+use ::tch::nn::{self, Embedding, LayerNorm, Linear, Module, Path};
+use ::tch::{Device, Kind, Tensor};
 
 pub struct TinyNlpTransformer {
   pub embed: Embedding,
@@ -20,13 +20,55 @@ impl TinyNlpTransformer {
     d_ff: i64,
     device: Device,
   ) -> Self {
-    todo!()
+    let embed = nn::embedding(vs / "embed", vocab, d_model, Default::default());
+
+    let block = EncoderBlock::new(&(vs / "enc0"), d_model, n_heads, d_ff);
+
+    let ln_f = nn::layer_norm(
+      vs / "ln_f",
+      vec![d_model],
+      nn::LayerNormConfig {
+        eps: 1e-5,
+        ..Default::default()
+      },
+    );
+
+    let head = nn::linear(vs / "head", d_model, 2, Default::default());
+
+    Self {
+      embed,
+      block,
+      ln_f,
+      head,
+      d_model,
+      device,
+    }
   }
 
   pub fn forward(
     &self,
     x_idx: &Tensor,
     train: bool,
+  ) -> Tensor {
+    let t = x_idx.size()[1];
+
+    let pe = Self::sinusoidal_pe(t, self.d_model, self.device);
+
+    let mut x = self.embed.forward(x_idx) + pe;
+
+    x = self.block.forward(&x, train);
+
+    let x = x
+      .apply(&self.ln_f)
+      .mean_dim([1].as_slice(), false, Kind::Float);
+
+    x.apply(&self.head)
+  }
+
+  fn sinusoidal_pe(
+    t_steps: i64,
+    d_model: i64,
+    device: Device,
   ) -> Tensor {
     todo!()
   }
